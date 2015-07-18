f580f10342f691ce96fb34ab7c7c876949ad7cc7
Joao Misko <joao.misko@mowa.com.br>
2015-07-18 19:57:42 -0300

    misko

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ var proxey = require('proxey');
 
 proxey.run({
 	rootFolder: './app',
-	port: 5000,
+	port: (process.env.PORT || 5000),
 	proxy: {
 	 '/goldark-api': 'http://vcengenharia.goldarkapi.com'
 	},
